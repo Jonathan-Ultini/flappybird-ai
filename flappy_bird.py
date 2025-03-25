de293229d5bdf00e7c82363a2a8382b2070b0cb4
Jonathan-Ultini <jonathanultini@gmail.com>
2025-03-25 09:38:58 +0100

    add move method to Bird class for improved jump physics

--- flappy_bird.py
+++ flappy_bird.py
@@ -19,7 +19,7 @@ BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png
 class Bird:
   IMGS = BIRD_IMGS
   # quanto il bird si inclinerà per salire
-  MAX_ROTATIONBN =  25
+  MAX_ROTATION =  25
   # rotazione ogni volta che si muove
   ROT_VEL = 20
   # qunato durerà ogni animazione
@@ -39,3 +39,26 @@ class Bird:
     self.vel = -10.5 # perchè per salire bisogna andare verso lo 0 in su e aumentare per scendere
     self.tick_count = 0 # sapre quando si cambia velocità o direzione
     self.height = self.y # da dove inizia a muoversi il bird
+
+  def move(self):
+    self.tick_count += 1 # teniamo conto delle volte che si muove
+
+    # di quanti pixel ci muoviamo, -10.5 + 1.5 = 1.5  continuando a salire o diminuire calcola quanto si muove nel salto
+    d = self.vel*self.tick_count + 1.5*self.tick_count**2
+
+    # controlliamo che non vada troppo veloce
+    if d >= 16:
+      d = 16
+
+    if d < 0:
+      d -= 2
+
+    self.y = self.y + d
+
+    if d < 0 or self.y < self.height + 50:
+      if self.tilt < self.MAX_ROTATION:
+        self.tilt = self.MAX_ROTATION
+      else:
+        if self.tilt > -90:
+          self.tilt -= self.ROT_VEL
+          
